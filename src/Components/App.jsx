9299643d2214ebbf65b54667ec272b4f0858b9cf
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-01-08 00:57:39 -0300

    Feat: Estilização Global

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,12 @@
+import { Header } from "./Header/Header";
+
+import './global.css'; 
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
   );
 }
