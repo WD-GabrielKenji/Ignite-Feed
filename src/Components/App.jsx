f9f0ffbbc18e9327b38f11a06e61f70630289e87
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-01-23 19:43:54 -0300

    Refactory: Componente Post (conteúdo e comentários)

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Post } from "./Posts/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/77596710?v=4',
+      name: 'Gabriel Kenji',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-10 20:17:00'),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -15,8 +46,16 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -1,9 +1,22 @@
 import styles from './Comment.module.css';
+import { useState } from 'react';
 
 import { Avatar } from '../Avatar/Avatar';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
-export function Comment(){
+export function Comment({ content, onDeleteComment }){
+  const [likeCount, setLikeCount] = useState(0);
+  
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+  
   return (
     <div className={styles.comment}>
       <Avatar 
@@ -25,18 +38,18 @@ export function Comment(){
               </time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Gabriel, parabéns!! 👏👏</p>
+          <p>{ content }</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
